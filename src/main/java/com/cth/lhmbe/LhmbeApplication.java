11019eee4c2a85badef88c1b9a3d9a4198a04de0
choitaehoon1998 <51841238+choitaehoon1998@users.noreply.github.com>
2022-10-22 02:26:30 +0900

    Initial: Initialize Project Create New Project

---
+++ src/main/java/com/cth/lhmbe/LhmbeApplication.java
@@ -0,0 +1,12 @@
+package com.cth.lhmbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LhmbeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LhmbeApplication.class, args);
+    }
+}
---
+++ src/test/java/com/cth/lhmbe/LhmbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cth.lhmbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LhmbeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
