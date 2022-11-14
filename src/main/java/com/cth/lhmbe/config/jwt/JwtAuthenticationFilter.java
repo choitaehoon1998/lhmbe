801ae4cb07ed253bfbf5dd0c8c4ead08aa6aa60e
choitaehoon1998 <51841238+choitaehoon1998@users.noreply.github.com>
2022-11-14 21:00:21 +0900

    테스트 코드 위하여 contextLoad 오류 제거 하기 위하여, application.yml 파일추가 , build.gradle 파일 수정

--- src/main/java/com/cth/lhmbe/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/cth/lhmbe/config/jwt/JwtAuthenticationFilter.java
@@ -6,10 +6,8 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.cth.lhmbe.config.auth.PrincipalDetails;
 import com.cth.lhmbe.dto.LoginRequestDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
 import java.util.Date;
 import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -20,7 +18,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
 	private final AuthenticationManager authenticationManager;
 
@@ -38,7 +36,7 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
 		UsernamePasswordAuthenticationToken authenticationToken =
 			new UsernamePasswordAuthenticationToken(
-				loginRequestDto.getEmail() + Character.LINE_SEPARATOR + loginRequestDto.getCompanyId() ,
+				loginRequestDto.getEmail() + Character.LINE_SEPARATOR + loginRequestDto.getCompanyId(),
 				loginRequestDto.getPassword());
 
 		Authentication authentication =
@@ -50,19 +48,19 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
 	@Override
 	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
-		Authentication authResult)  {
+		Authentication authResult) {
 
 		PrincipalDetails principalDetailis = (PrincipalDetails) authResult.getPrincipal();
 
 		String jwtToken = JWT.create()
 			.withSubject(principalDetailis.getUsername())
-			.withExpiresAt(new Date(System.currentTimeMillis()+JwtProperties.EXPIRATION_TIME))
+			.withExpiresAt(new Date(System.currentTimeMillis() + JwtProperties.EXPIRATION_TIME))
 			.withClaim("email", principalDetailis.getEmail())
 			.withClaim("username", principalDetailis.getUsername())
 			.withClaim("companyId", principalDetailis.getCompanyId())
 			.sign(Algorithm.HMAC512(JwtProperties.SECRET));
 
-		response.addHeader(JwtProperties.HEADER_STRING, JwtProperties.TOKEN_PREFIX+jwtToken);
+		response.addHeader(JwtProperties.HEADER_STRING, JwtProperties.TOKEN_PREFIX + jwtToken);
 	}
 
 }
